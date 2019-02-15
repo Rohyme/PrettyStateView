673adf2b26aa16f35b1ea3af4ffe67b36bf43e6b
rohyme <eng.mhmd.rohyme@gmail.com>
2019-02-15 17:56:13 +0200

    Fixing layout problems
    -  Fixing constraint layout problems
    -  Fixing conflicts happened when there is two views in the same parent

--- prettystates/src/main/java/com/tripl3dev/prettystates/StateExecuter.kt
+++ prettystates/src/main/java/com/tripl3dev/prettystates/StateExecuter.kt
@@ -1,63 +1,81 @@
 package com.tripl3dev.prettystates
 
 import android.content.Context
-import android.support.v7.widget.RecyclerView
+import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TableLayout
 
 
 fun View.setState(stateType: Int): View {
     Log.i("STATE_CHANGED", "State Type  =  $stateType")
     val stateConfig = StatesConfigFactory.instance
     lateinit var stateView: View
+    val currentViewId = this.id
+    val stateViewId = currentViewId * 3
     stateView = if (stateType == StatesConstants.NORMAL_STATE) {
-        View(this.context)
+        this
     } else {
         stateConfig!!.getStateView(stateType).inflateToView(this.context)
     }
-    var parentView: ViewGroup? = null
-
-    if (this.parent is ViewGroup && this.parent !is RecyclerView) {
-        parentView = this.parent as ViewGroup
+    var parentView: ViewGroup? = this.parent as ViewGroup
+    while (!parentView?.isLayoutContainer()!!) {
+        parentView = parentView.parent as ViewGroup?
     }
-    if (parentView != null) {
-        val view: View? = parentView.findViewById(StatesConstants.STATE_VIEW_ID)
-        if (view != null) {
-            view.visibility = View.GONE
-        }
-        if (stateType != StatesConstants.NORMAL_STATE) {
-            stateView.id = StatesConstants.STATE_VIEW_ID
-            stateView.layoutParams = this.layoutParams
-            if (view == null) {
-                if (stateView.parent != null)
-                    (stateView.parent as ViewGroup).removeView(stateView) // <- fix
 
-                if (parentView is LinearLayout) {
-                    parentView.addView(stateView, parentView.indexOfChild(this))
-                } else {
-                    parentView.addView(stateView)
-                }
+
+    val view: View? = parentView.findViewById(stateViewId)
+    if (view != null) {
+        parentView.removeViewInLayout(view)
+    }
+    if (stateType != StatesConstants.NORMAL_STATE) {
+        stateView.id = stateViewId
+        when (parentView) {
+            is ConstraintLayout -> {
+                parentView.addView(stateView)
+                val set = ConstraintSet()
+                set.clone(parentView)
+                set.connect(stateView.id, ConstraintSet.LEFT, this.id, ConstraintSet.LEFT, 4)
+                set.connect(stateView.id, ConstraintSet.RIGHT, this.id, ConstraintSet.RIGHT, 4)
+                set.connect(stateView.id, ConstraintSet.TOP, this.id, ConstraintSet.TOP, 4)
+                set.connect(stateView.id, ConstraintSet.BOTTOM, this.id, ConstraintSet.BOTTOM, 4)
+                set.applyTo(parentView)
             }
-            else {
-                val index = parentView.indexOfChild(view)
-                parentView.removeView(view)
-                parentView.addView(stateView, index)
-                stateView.visibility = View.VISIBLE
+            is LinearLayout -> {
+                parentView.addView(stateView, parentView.indexOfChild(this))
+                stateView.layoutParams = this.layoutParams
             }
-            if (this.visibility == View.VISIBLE) {
-                this.visibility = View.GONE
-            }
-        } else {
-            if (this.visibility == View.GONE) {
-                this.visibility = View.VISIBLE
+            else -> {
+                parentView.addView(stateView)
+                stateView.layoutParams = this.layoutParams
             }
         }
 
+
+        if (this.visibility == View.VISIBLE) {
+            this.visibility = View.INVISIBLE
+        }
+    } else {
+        if (this.visibility == View.INVISIBLE) {
+            this.visibility = View.VISIBLE
+        }
     }
+
     return stateView
+}
+
+fun View.isLayoutContainer(): Boolean {
+    return this is LinearLayout ||
+            this is RelativeLayout ||
+            this is FrameLayout ||
+            this is ConstraintLayout ||
+            this is TableLayout
 }
 
 fun Int.inflateToView(context: Context): View {
